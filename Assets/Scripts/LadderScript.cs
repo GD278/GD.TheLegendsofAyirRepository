bcdcab85ceb77324a576553d721ae88510e346cd
GD278 <57197845+GD278@users.noreply.github.com>
2020-04-29 19:05:28 -0500

    Add Ladder Script
    Added Ladder Script

---
+++ Assets/Scripts/LadderScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float speed;
+    bool byLadder = false;
+    private Rigidbody2D playerRigidbody;
+    [SerializeField] private GameObject player;
+    void Start()
+    {
+        playerRigidbody = player.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (byLadder && Input.GetKeyDown("W"))
+        {
+            playerRigidbody.velocity = new Vector3(0, 1, 0);
+        }
+        if (byLadder && Input.GetKeyDown("S"))
+        {
+            playerRigidbody.velocity = new Vector3(0, -1, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+        if (collision.gameObject.name == "Player")
+        {
+            byLadder = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            byLadder = false;
+        }
+    }
+}
