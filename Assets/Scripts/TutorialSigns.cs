a7ed3859df4d3caa89904d9b036da8e9aaceb8a3
GD278 <57197845+GD278@users.noreply.github.com>
2020-05-05 09:25:17 -0500

    Added a Script for Tutorial Sign Display

---
+++ Assets/Scripts/TutorialSigns.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TutorialSigns : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private TextMeshProUGUI tutorialText;
+    [SerializeField] private string tutorialString;
+    void Start()
+    {
+        tutorialText.enabled = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+
+            Debug.Log("Entered trigger.");
+            tutorialText.SetText(tutorialString);
+            tutorialText.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Exited Trigger");
+
+            tutorialText.enabled = false;
+        }
+    }
+}
