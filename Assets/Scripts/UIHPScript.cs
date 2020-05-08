77d858e304393a672c913709a8af83cacd2bd4fb
GD278 <57197845+GD278@users.noreply.github.com>
2020-05-08 10:26:56 -0500

    Added a UI Element to Track Players HP

---
+++ Assets/Scripts/UIHPScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIHPScript : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI hpText;
+    PlayerCombat playerCombat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCombat = GetComponent<PlayerCombat>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerCombat.currentHealth < 0)
+		{
+            playerCombat.currentHealth = 0;
+		}
+        hpText.SetText($"HP: {playerCombat.currentHealth}");
+    }
+}
