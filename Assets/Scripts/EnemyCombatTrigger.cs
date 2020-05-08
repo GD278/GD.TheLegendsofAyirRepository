6a338938c39df20ed10b20f8b1f1c7fac21e26c2
GD278 <57197845+GD278@users.noreply.github.com>
2020-05-07 19:59:37 -0500

    Keeps track of how many times player has entered trigger
    This is used to only play the skeleton vocal once.

--- Assets/Scripts/EnemyCombatTrigger.cs
+++ Assets/Scripts/EnemyCombatTrigger.cs
@@ -6,12 +6,16 @@ public class EnemyCombatTrigger : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private GameObject enemy;
+    AudioSource source;
+    [SerializeField] private AudioClip [] enterCombat;
+    int timesEntered;
     
     public bool inCombat;
     void Start()
     {
         inCombat = false;
-        
+        source = enemy.GetComponent<AudioSource>();
+        timesEntered = 0;
     }
 
     // Update is called once per frame
@@ -24,8 +28,19 @@ public class EnemyCombatTrigger : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            Debug.Log("You have entered combat.");
-            inCombat = true;
+
+            if (timesEntered == 0)
+            {
+                source.clip = enterCombat[Random.Range(0, enterCombat.Length)];
+                source.Play();
+                timesEntered++;
+                inCombat = true;
+            }
+            else
+            {
+                Debug.Log("You have entered combat.");
+                inCombat = true;
+            }
         }
     }
     private void OnTriggerExit2D(Collider2D other)
