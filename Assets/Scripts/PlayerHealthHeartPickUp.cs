1f6b05e7f4b653959d1a400e323dfb8f50d8f27a
GD278 <57197845+GD278@users.noreply.github.com>
2020-05-08 13:15:30 -0500

    Added in hearts that restore players health
    Added in hearts that restore players health when running over them.

---
+++ Assets/Scripts/PlayerHealthHeartPickUp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthHeartPickUp : MonoBehaviour
+{
+    PlayerCombat playerCombat;
+     private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerCombat = player.GetComponent<PlayerCombat>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+	{
+        if(other.gameObject.name == "Player" && playerCombat.currentHealth < playerCombat.maxHealth)
+		{
+            playerCombat.currentHealth = playerCombat.currentHealth + 25;
+            Destroy(gameObject);
+            if(playerCombat.currentHealth > playerCombat.maxHealth)
+			{
+                playerCombat.currentHealth = playerCombat.maxHealth;
+			}
+        }
+	}
+}
