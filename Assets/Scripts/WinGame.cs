5ca4acf4262af31eb24aaa35e9334ad0fff8c4f5
GD278 <57197845+GD278@users.noreply.github.com>
2020-05-16 17:25:31 -0500

    Built Level 3 and added a way to win the game

---
+++ Assets/Scripts/WinGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WinGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private TextMeshProUGUI youWinTB;
+    PlayerController controller;
+    [SerializeField] private GameObject Player;
+    void Start()
+    {
+        controller = Player.GetComponent<PlayerController>();
+        youWinTB.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+	{
+        if(collision.gameObject.name == "Player")
+		{
+            controller.canUseController2 = false;
+            youWinTB.enabled = true;
+		}
+	}
+}
