03b05823977ed99fe4c73993c912f638c3ae9a52
GD278 <57197845+GD278@users.noreply.github.com>
2020-05-17 08:21:44 -0500

    Added Story Context to Game

---
+++ Assets/Scripts/StoryContextScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryContextScript : MonoBehaviour
+{
+    PlayerController controller;
+    [SerializeField] private GameObject LivingPanel;
+    [SerializeField] private GameObject StoryPanel;
+    [SerializeField] private GameObject Player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = Player.GetComponent<PlayerController>();
+        controller.canUseController2 = false;
+        LivingPanel.SetActive(false);
+        StoryPanel.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CloseSM()
+	{
+        controller.canUseController2 = true;
+        StoryPanel.SetActive(false);
+        LivingPanel.SetActive(true);
+	}
+}
