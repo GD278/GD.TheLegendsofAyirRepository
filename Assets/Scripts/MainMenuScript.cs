4b9f83100bc363907d366aefa3ac5be012203052
GD278 <57197845+GD278@users.noreply.github.com>
2020-05-17 11:13:35 -0500

    Organized and Made Minor stat changes

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    [SerializeField] private GameObject MainMenu;
+    [SerializeField] private GameObject CreditsMenu;
+    // Start is called before the first frame update
+    void Start()
+	{
+       ShowMainMenu();
+	}
+    public void ShowMainMenu()
+	{
+        MainMenu.SetActive(true);
+        CreditsMenu.SetActive(false);
+    }
+    public void ShowCredits()
+	{
+        MainMenu.SetActive(false);
+        CreditsMenu.SetActive(true);
+    }
+
+    public void Play()
+	{
+        SceneManager.LoadScene("Level01");
+	}
+    public void Quit()
+	{
+        Application.Quit(0);
+	}
+}
